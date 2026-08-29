949b0dbe184968a9f77a1a101cfaa33b2682bc15
Santosh-Nepali <santoshtechguy@gmail.com>
2026-08-29 09:45:55 +0300

    program_1 done with readme file with code

--- mod03/program_1.py
+++ mod03/program_1.py
@@ -9,17 +9,19 @@ how many centimeters below the size limit the caught fish was.
 
 zander_size_limit=47 # variable setting the limit of fish size to catch
 
-print("======= Hello Fisherman ======= ")
-while True:
-    try:
-        zander_size=float(input('Enter the lengthe of Zander you catch in centimeter >>> '))
-        break
-    except ValueError:
-        print('Value is valid ')
-        continue
+print("======= Hello Fisherman ======= ")  # display text
 
-if zander_size<zander_size_limit:       
-    print('Sorry!! Fisherman Effort is appreciated')
-    print(f'Release the fish back to lake as fish is {(zander_size_limit-zander_size)} cm below standard limit >=47 cm')
+while True:   # looping until it holds true
+    try:
+        zander_size=float(input('Enter the lengthe of Zander you catch in centimeter >>> '))   # asking length of fish and converting into float datatype
+        break                           #exit the loop 
+
+    except ValueError:                  #value error  
+        print('Value is valid ')        #displays valid 
+        continue                        # continue the loop
+
+if zander_size<zander_size_limit:       #Checking the size of standar limit
+    print('Sorry!! Fisherman Effort is appreciated') #display the message
+    print(f'Release the fish back to lake as fish is {(zander_size_limit-zander_size)} cm below standard limit >=47 cm') #displays the message with size of fish short of standard limits
 else:
-    print(f'Congratulation Fisherman you catch {zander_size} cm fish from lake')
+    print(f'Congratulation Fisherman you catch {zander_size} cm fish from lake')   # displays the message with size of fish catch by fisherman
