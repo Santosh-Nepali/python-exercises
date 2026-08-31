d3cb79cd79dea087db591ad74dece9e9ea7760a8
Santosh-Nepali <santoshtechguy@gmail.com>
2026-08-31 11:34:05 +0300

    module4 program2 completed

--- mod04/program_2.py
+++ mod04/program_2.py
@@ -4,3 +4,9 @@ Write a program that converts inches to centimeters
 until the user inputs a negative value. Then the program ends.
 
 """
+inches=float(input('Enter the value for inch'))
+while inches>0:
+    centimeter=inches*2.54
+    print(f'{inches:0.2f} inches is equals to {centimeter:0.2f} centimeter')
+    inches=float(input('Enter negative value to stop the program'))
+print('programs ended')
