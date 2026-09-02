81123db8133fc9b62c289b4fda24b017231f7f4c
santsh <san@santshs-iMac.local>
2026-09-02 17:38:53 +0300

    defining main menu

--- project/main.py
+++ project/main.py
@@ -3,6 +3,12 @@ Project 1. Starting the Programming Project Assignment
 Create a separate folder project/ for the game inside the Python exercise project, and create a readme.md file inside it. Add the name of your game as the heading and your own name below it.
 Create a program in the folder that asks for the player’s name and age, stores them in variables, and prints them to the console.
 
+
+Project 2. Main Menu
+Modify the game project program so that if the user enters an age under 12, the program informs them that they are a minor and shuts down.
+Otherwise, the program greets the user, displays the main menu, and asks for commands until the user enters "lopeta".
+Add a few fictional commands that each produce a different output in the console. After a command, always display the menu again.
+
 """
 def user_info():
     fname=input('Enter your first name ')
