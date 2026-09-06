0fb255d380deacacca541118653363cdcdeb7a78
Santosh-Nepali <santoshtechguy@gmail.com>
2026-09-07 01:17:57 +0300

    module 6 program6

--- mod06/program_6.py
+++ mod06/program_6.py
@@ -8,15 +8,35 @@ and tells the user which pizza provides better value for money (which of them ha
 You must use the function you wrote for calculating the unit prices.
 
 """
+import math
 
+#  user defined function  for unit price calculator
 def unit_price_calculator(diameter_cm, price_pizza_euro):
-    pass
+    radius=diameter_cm/2
+    area_m=(math.pi*radius**2)/10000
+    return price_pizza_euro/area_m
 
+# user-defined function of unit price per square meter
+unit_price_per_sq_meter=[]
+for i in range(2):
+    diameter=float(input(f'Enter diameter of {i+1} pizza in cm::: '))
+    price=float(input(f'Enter the price of {i+1} pizza in Euro:::  '))
+    unit_price=unit_price_calculator(diameter, price)
+    unit_price_per_sq_meter.append(unit_price)
 
+for count, pizza_price in enumerate(unit_price_per_sq_meter):
+    print(f'Price of Pizza {count+1} is {pizza_price:0.2f}')
+    
 
-
-
-
+if unit_price_per_sq_meter[0]<unit_price_per_sq_meter[1]:
+    print('First Pizza has better value for money')
+    
+elif unit_price_per_sq_meter[0]>unit_price_per_sq_meter[1]:
+    print('Second Pizza has better value for money')
+    
+else:
+    print('Both Pizza has same value')
+    
 
 
 
