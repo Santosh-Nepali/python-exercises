9bef5f2338a7838750ff5b8e35fa2ccfadc39e01
Santosh-Nepali <santoshtechguy@gmail.com>
2026-09-21 00:52:55 +0300

    module09 and program2 done

--- mod09/program2.py
+++ mod09/program2.py
@@ -1,61 +1,112 @@
 """
-Module_8 program2.py
+Module 9 program 2
+Extend the previous program by creating a Building class.
+The initializer parameters for the class are the numbers of the bottom and top floors and the number of elevators in the building.
+When a building is created, the building creates the required number of elevators.
+The list of elevators is stored as a property of the building.
+Write a method called run_elevator that accepts the number of the elevator and the destination floor as its parameters.
+In the main program, write the statements for creating a new building and running the elevators of the building.
 
-Extend the program by adding an accelerate method into the new class.
-The method should receive the change of speed (km/h) as a parameter.
-If the change is negative, the car reduces speed. 
-The method must change the value of the speed property of the object. 
-The speed of the car must stay below the set maximum and cannot be less than zero.
-Extend the main program so that the speed of the car is first increased by +30 km/h, then +70 km/h and finally +50 km/h.
-Then print out the current speed of the car.
-Finally, use the emergency brake by forcing a -200 km/h change on the speed and then print out the final speed.
-The travelled distance does not have to be updated yet.
+
+
+
 """
-# Creating class car with attributes and methods
-class Car:
-    def __init__(self, registration_no, max_speed): #constructor initializer
-        self.registration_no=registration_no
-        self.max_speed=max_speed
-        self.current_speed=0
-        self.travel_distance=0
+
+#class called Elevator with attribute bottom floor and top floor
+class Elevator:
+    def __init__(self, bottom_floor, top_floor): # constructor initializer which get object(self), bottom_floor and top_floor at attribute
+        self.bottom_floor=bottom_floor
+        self.top_floor=top_floor
+        self.current_floor=self.bottom_floor
+            
+    def floor_up(self):
+        self.current_floor+=1
+        #print(f'Current floor :: {self.current_floor}')
+
+    def floor_down(self):
+        self.current_floor-=1
+        #print(f'Current Floor :: {self.current_floor}')
         
-    def accelerate(self,change_in_speed):
-        self.change_in_speed=change_in_speed
-        self.current_speed=self.current_speed+self.change_in_speed
-        #print(self.current_speed)
+    
+    def go_to_floor(self,target_floor):
+        self.target_floor=target_floor
+        #while True:   
+        while self.current_floor!=self.target_floor:
+            if self.current_floor<self.target_floor:
+                self.floor_up()
+            else:
+                self.floor_down()
+
+#defining class Building with different attributes
+class Building:
+    def __init__(self, bottom_floor, top_floor, no_of_elevators):
+        self.bottom_floor=bottom_floor
+        self.top_floor=top_floor
+        self.no_of_elevators=no_of_elevators
         
-        # Checking condition for current_speed must below max_spped
-        if self.current_speed>self.max_speed:
-            print("**** Alert!! you might get overspeed fine. ****\n Your's Current speed :: ",self.current_speed,"km/h")
-            self.current_speed=self.max_speed
-            #print("Good Job, You maintain the maximum speed i.e. ",self.current_speed,"km/h")
+        self.elevators_collection=[]
+        # here number of elevators looping and create objects with top floor and bottom floor
+        for count in range(self.no_of_elevators):
+            self.elevator=Elevator(self.bottom_floor, self.top_floor)
+            self.elevators_collection.append(self.elevator)
+            
+    def run_elevator(self, target_elevator_no, target_floor):
+        self.target_floor=target_floor
+        self.target_elevator=target_elevator_no
         
-        # Checking condition for speed in negative or not. 
-        if self.current_speed<0:
-            self.current_speed=0
-            #print("The speed cannot be negative value")
+        elevator=self.elevators_collection[self.target_elevator]
+        elevator.go_to_floor(target_floor)
+    
+    
+    
+# Begining of main program
+no_of_elevators=6
+top_floor=12
+bottom_floor=0
+
+#objects of class Building with number of floors and number of elevators
+building=Building(bottom_floor,top_floor,no_of_elevators) 
+
+
+ 
+#accessing the objects of elevators 
+for number, elevator in enumerate(building.elevators_collection):
+    print(f'{number}  {elevator}')               
+
+# Asking which elevators to use 
+while True:
+    try:
+        target_elevator_no=int(input("which Elevator you want to use? [1-6]"))
         
-     
-# Main program
-# creating objects of the class .    
-car=Car("ABC-123", 142)
+        if target_elevator_no>=1 and target_elevator_no<=6:
+            break
+        
+        else:
+            print("Elevator does not exist.")
+    
+    except ValueError:
+        print("Invalid input")
+        
+        
+#asking target floor to reach    
+while True:
+    try:
+        target_floor=int(input('Which floor would you like to go?[0 - 12] '))
+ 
+        if target_floor>=0 and target_floor<=12:
+            #elevator.go_to_floor(target_floor)
+            #print(f'You have reached to {elevator.current_floor}')
+            break
+        else:
+           print("Floor does not exist.")
+        
+    except ValueError:
+       print('invalid input')  
+    
+# calling method of class Building and passing elevator number and floor number as attributes
+building.run_elevator(target_elevator_no,target_floor)
 
-# Diplaying current details of the car
-print("Registration Number: ",car.registration_no)
-print(" - Maximum Speed: ", car.max_speed)
-print(" - Current Speed: ", car.current_speed)
-print(" - Travelled Distance: ",car.travel_distance)
+#Displaying the result of reached floor using specific elevator
+print(f'You have to reached to the floor {building.elevator.current_floor} using elevator {building.target_elevator} ')
 
-#Accelerating
-car.accelerate(30)
-print("The current Speed after accelerating +30 km/h :: ",car.current_speed,"km/h")
-car.accelerate(70)
-print("The current Speed after accelerating +70 km/h ::",car.current_speed,"km/h")
-car.accelerate(50)
-print("The current Speed after accelerating +50 km/h ::",car.current_speed,"km/h")
-
-# Emergency brake
-
-car.accelerate(-200)
-print("The final speed after Applying Emergency brake :: ",car.current_speed,"km/h")
-
+        
